9fd63ff47ffac42b6dff2aa34e638c99146e1458
Nikhil T D <63257727+dnikhil0306@users.noreply.github.com>
2024-12-06 22:30:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,15 +13,18 @@ require('dotenv').config();
 
 const app = express();
 
-// Middleware
-app.use(bodyParser.json());
-// app.use(cors());
-app.use(express.json());
+
 app.use(cors({
   origin: 'https://giftcards-client.onrender.com',  // Allow requests from this domain
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
+// Middleware
+app.use(bodyParser.json());
+// app.use(cors());
+app.use(express.json());
+
 
 
 
