d1a854c81685530696bc1395c92af870f54f7cdf
Nikhil T D <63257727+dnikhil0306@users.noreply.github.com>
2024-12-06 22:00:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,8 +15,13 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: 'https://giftcards-client.onrender.com',  // Allow requests from this domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 
 
