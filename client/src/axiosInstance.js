40871125972f826e267f579be7dba656fd196c80
Nikhil T D <63257727+dnikhil0306@users.noreply.github.com>
2024-12-06 21:05:42 +0530

    Update axiosInstance.js

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api/orders', // Update with your server's base URL
-    // baseURL: 'http://localhost:5000', // Update with your server's base URL
+    // baseURL: 'http://localhost:5000/api/orders', // Update with your server's base URL
+    baseURL: 'https://giftcards-server.onrender.com/api/orders', // Update with your server's base URL
     headers: {
         'Content-Type': 'application/json',
     },
